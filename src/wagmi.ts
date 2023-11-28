e100588955786c4b49329098696bde1b2b99152a
yanfeizuo <hh18856856693@gmail.com>
2023-11-28 17:43:20 +0800

    add meter

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -5,8 +5,52 @@ import { publicProvider } from 'wagmi/providers/public';
 
 const walletConnectProjectId = '176492253f1830f33ad81110be200648';
 
+const meter = {
+  id: 82,
+  name: "Meter",
+  network: "meter",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Meter Stable",
+    symbol: "MTR",
+  },
+  rpcUrls: {
+    default: { http: ["https://rpc.meter.io"] },
+    public: { http: ["https://rpc.meter.io"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "MeterScan", url: "https://scan.meter.io" },
+    default: { name: "MeterScan", url: "https://scan.meter.io" },
+  },
+  contracts: {},
+};
+
+const meterTestnet = {
+  id: 83,
+  name: "Meter Testnet",
+  network: "meterTestnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Meter Stable",
+    symbol: "MTR",
+  },
+  rpcUrls: {
+    default: { http: ["https://rpctest.meter.io"] },
+    public: { http: ["https://rpctest.meter.io"] },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: "MeterScan",
+      url: "https://scan-warringstakes.meter.io",
+    },
+    default: { name: "MeterScan", url: "https://scan-warringstakes.meter.io" },
+  },
+  contracts: {},
+  testnet: true,
+};
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
+  [mainnet, meter, meterTestnet],
   [publicProvider()]
 );
 
